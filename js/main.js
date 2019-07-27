7542ac3326f5578950f9bf72a37dfcc4a5744d1a
nhasley <novahasley@gmail.com>
2019-07-27 18:45:03 -0500

    Added init function

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 /*----- constants -----*/
 const COLORS = {
-    "0"="White",
-    "1"="Red",
-    "-1"="Blue"
-}
+  "0": "White",
+  "1": "Red",
+  "-1": "Blue"
+};
+
 /*----- app's state (variables) -----*/
+let board, turn, winner;
 
 /*----- cached element references -----*/
+init();
+
+function init() {
+  board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  turn = 1;
+  winner = null;
+  render();
+}
 
 /*----- event listeners -----*/
 
